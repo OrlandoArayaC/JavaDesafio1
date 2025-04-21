de7807427f721a8b448933d267c5fa35f8c8cd08
Orlando Araya <o.arayacalderon@gmail.com>
2025-04-21 16:36:14 -0400

    finalizado

--- assets/index.js
+++ assets/index.js
@@ -1,28 +1,30 @@
-precio = 400000
-cantidad = 0
-total = 0
 
+let precio = 400000;
+let cantidad = 0;
 
-valorTotal = document.querySelector(".valor-total")
-valorTotal.innerHTML = total
-precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
-cantidadSpan = document.querySelector(".cantidad")
+const precioSpan = document.querySelector(".precio-inicial");
+const cantidadSpan = document.querySelector(".cantidad");
+const valorTotal = document.querySelector(".valor-total");
+const btnMas = document.querySelector(".btnMas");
+const btnMenos = document.querySelector(".btnMenos");
 
-btnMas = document.querySelector(".btnMas")
-btnMenos = document.querySelector(".btnMenos")
+precioSpan.textContent = precio;
+valorTotal.textContent = 0;
 
-btnMas.addEventListener ("click", () => {
-    cantidad = cantidad + 1
-    cantidadSpan.innerHTML = cantidad
-    valorTotal.innerHTML = precio * cantidad
-})
+function actualizarTotal() {
+    valorTotal.textContent = precio * cantidad;
+}
 
-bntMenos.addEventListener ("click", () => {
-    cantidad = cantidad - 1
-    if (cantidad < 0) {
-        cantidad = 0
+btnMas.addEventListener("click", () => {
+    cantidad++;
+    cantidadSpan.textContent = cantidad;
+    actualizarTotal();
+});
+
+btnMenos.addEventListener("click", () => {
+    if (cantidad > 0) {
+        cantidad--;
+        cantidadSpan.textContent = cantidad;
+        actualizarTotal();
     }
-    cantidadSpan.innerHTML = cantidad
-    valorTotal.innerHTML =  precio * cantidad
-})
+});
